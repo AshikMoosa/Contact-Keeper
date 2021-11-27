dda8f4067973f501da5978aff39b3ed4093aa427
MysticMoz <ashikmoosa96@gmail.com>
2021-11-27 12:56:49 +0530

    Configured backend for adding contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,25 +1,39 @@
-import React, { useContext, Fragment } from "react";
+import React, { useContext, Fragment, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
+import Spinner from "../layout/Spinner";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts ,filtered} = contactContext;
+  const { contacts, filtered, loading, getContacts } = contactContext;
 
-  if(contacts.length === 0 ) {
-    return (<h4>Please add a contact</h4>);
+  useEffect(() => {
+    getContacts();
+    //eslint-disable-next-line
+  }, []);
+
+  if (contacts !== null && contacts.length === 0 && !loading) {
+    return <h4>Please add a contact</h4>;
+  } else {
+    return (
+      <Fragment>
+        {contacts !== null && !loading ? (
+          <>
+            {filtered !== null
+              ? filtered.map((contact) => (
+                  <ContactItem key={contact._id} contact={contact} />
+                ))
+              : contacts.map((contact) => (
+                  <ContactItem key={contact._id} contact={contact} />
+                ))}
+          </>
+        ) : (
+          <Spinner />
+        )}
+      </Fragment>
+    );
   }
-  else {
-return (
-    <Fragment>
-      {filtered !== null ? filtered.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />)) : contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />))}
-    </Fragment> 
-  );
-  }
-  
 };
 
 export default Contacts;
