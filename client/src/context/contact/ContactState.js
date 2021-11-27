b0a9b8c725db48df7a017b2c1a400d72816b552c
MysticMoz <ashikmoosa96@gmail.com>
2021-11-27 17:23:31 +0530

    Configured Update & Edit Contacts from backend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -53,10 +53,10 @@ const ContactState = (props) => {
   };
 
   //Delete Contact
-  const deleteContact = async (id) => {
+  const deleteContact = async (contact) => {
     try {
-      await axios.delete(`/api/contacts/${id}`);
-      dispatch({ type: DELETE_CONTACT, payload: id });
+      await axios.delete(`/api/contacts/${contact._id}`);
+      dispatch({ type: DELETE_CONTACT, payload: contact._id });
     } catch (err) {
       dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
     }
@@ -78,8 +78,24 @@ const ContactState = (props) => {
   };
 
   //Update Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+    //dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Filter Contacts
